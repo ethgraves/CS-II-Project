fd6eaad16c9abddc1d0da2dcfb3b698b3ffae395
ethgraves <ethgraves05@gmail.com>
2024-04-21 18:25:43 -0500

    Calculator (Version 1.1: Added all number buttons)

--- Project 1 - Calculator/logic.py
+++ Project 1 - Calculator/logic.py
@@ -11,6 +11,13 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.button_0.clicked.connect(lambda : self.push_0())
         self.button_1.clicked.connect(lambda : self.push_1())
         self.button_2.clicked.connect(lambda : self.push_2())
+        self.button_3.clicked.connect(lambda : self.push_3())
+        self.button_4.clicked.connect(lambda : self.push_4())
+        self.button_5.clicked.connect(lambda : self.push_5())
+        self.button_6.clicked.connect(lambda : self.push_6())
+        self.button_7.clicked.connect(lambda : self.push_7())
+        self.button_8.clicked.connect(lambda : self.push_8())
+        self.button_9.clicked.connect(lambda : self.push_9())
 
 
     def length_check(self):
@@ -31,9 +38,8 @@ class Logic(QMainWindow, Ui_MainWindow):
             Logic.nums_in_box[0] = '0'
         else:
             if self.length_check() == True:
-                self.label_calculations.setText(f'0{''.join(Logic.nums_in_box)}')
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}0')
                 Logic.nums_in_box.append('0')
-        print(Logic.nums_in_box)
 
     def push_1(self):
         if self.box_empty_check() == True:
@@ -41,9 +47,8 @@ class Logic(QMainWindow, Ui_MainWindow):
             Logic.nums_in_box[0] = '1'
         else:
             if self.length_check() == True:
-                self.label_calculations.setText(f'1{''.join(Logic.nums_in_box)}')
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}1')
                 Logic.nums_in_box.append('1')
-        print(Logic.nums_in_box)
 
     def push_2(self):
         if self.box_empty_check() == True:
@@ -51,6 +56,68 @@ class Logic(QMainWindow, Ui_MainWindow):
             Logic.nums_in_box[0] = '2'
         else:
             if self.length_check() == True:
-                self.label_calculations.setText(f'2{''.join(Logic.nums_in_box)}')
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}2')
                 Logic.nums_in_box.append('2')
-        print(Logic.nums_in_box)
+
+    def push_3(self):
+        if self.box_empty_check() == True:
+            self.label_calculations.setText('3')
+            Logic.nums_in_box[0] = '3'
+        else:
+            if self.length_check() == True:
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}3')
+                Logic.nums_in_box.append('3')
+
+    def push_4(self):
+        if self.box_empty_check() == True:
+            self.label_calculations.setText('4')
+            Logic.nums_in_box[0] = '4'
+        else:
+            if self.length_check() == True:
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}4')
+                Logic.nums_in_box.append('4')
+
+    def push_5(self):
+        if self.box_empty_check() == True:
+            self.label_calculations.setText('5')
+            Logic.nums_in_box[0] = '5'
+        else:
+            if self.length_check() == True:
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}5')
+                Logic.nums_in_box.append('5')
+
+    def push_6(self):
+        if self.box_empty_check() == True:
+            self.label_calculations.setText('6')
+            Logic.nums_in_box[0] = '6'
+        else:
+            if self.length_check() == True:
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}6')
+                Logic.nums_in_box.append('6')
+
+    def push_7(self):
+        if self.box_empty_check() == True:
+            self.label_calculations.setText('7')
+            Logic.nums_in_box[0] = '7'
+        else:
+            if self.length_check() == True:
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}7')
+                Logic.nums_in_box.append('7')
+
+    def push_8(self):
+        if self.box_empty_check() == True:
+            self.label_calculations.setText('8')
+            Logic.nums_in_box[0] = '8'
+        else:
+            if self.length_check() == True:
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}8')
+                Logic.nums_in_box.append('8')
+
+    def push_9(self):
+        if self.box_empty_check() == True:
+            self.label_calculations.setText('9')
+            Logic.nums_in_box[0] = '9'
+        else:
+            if self.length_check() == True:
+                self.label_calculations.setText(f'{''.join(Logic.nums_in_box)}9')
+                Logic.nums_in_box.append('9')
